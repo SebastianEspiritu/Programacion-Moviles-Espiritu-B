3bcb11ac9f510a762f1e42bffc2ed7aced0b9526
SebastianEspiritu <sebastian.espiritu@tecsup.edu.pe>
2026-09-16 14:58:10 -0500

    feat(semana-04): creacion del composable ItemTarea con Checkbox y Boton Eliminar

--- semana-04/LaboratorioEstados/app/src/main/java/com/espiritu/laboratorioestados/PantallaTareas.kt
+++ semana-04/LaboratorioEstados/app/src/main/java/com/espiritu/laboratorioestados/PantallaTareas.kt
@@ -18,3 +18,46 @@ data class Tarea(
     val nombre: String,
     val completada: Boolean = false
 )
+
+@Composable
+fun ItemTarea(
+    tarea: Tarea,
+    onEliminar: () -> Unit,
+    onCambiarEstado: (Boolean) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                modifier = Modifier.weight(1f)
+            ) {
+                Checkbox(
+                    checked = tarea.completada,
+                    onCheckedChange = {
+                        onCambiarEstado(it)
+                    }
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Text(
+                    text = tarea.nombre,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(top = 12.dp)
+                )
+            }
+
+            Button(onClick = onEliminar) {
+                Text("Eliminar")
+            }
+        }
+    }
+}
