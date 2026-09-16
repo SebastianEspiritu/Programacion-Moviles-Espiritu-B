3bd3488f397db56c91ce1c7bc8133c246df2fb9a
SebastianEspiritu <sebastian.espiritu@tecsup.edu.pe>
2026-09-16 14:33:17 -0500

    feat(semana-04): creacion de TemperatureDisplay, declaracion de estado temperatura con remember y visualizacion en Text

---
+++ semana-04/LaboratorioEstados/app/src/main/java/com/espiritu/laboratorioestados/TemperatureDisplay.kt
@@ -0,0 +1,28 @@
+package com.espiritu.laboratorioestados
+
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TemperatureDisplay() {
+    var temperatura by remember { mutableStateOf(20) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Temperatura: $temperatura°C",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
