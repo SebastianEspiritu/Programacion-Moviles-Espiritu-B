b398537ffb62ce83e99f22aee70be5c172480866
Sebastian Espiritu <sebastian.espiritu@tecsup.edu.pe>
2026-09-23 11:26:55 -0500

    crear FitrnessClass y Resservation models

--- semana-05/TecsupFIT/app/src/main/java/com/espiritu/tecsupfit/MainActivity.kt
+++ semana-05/TecsupFIT/app/src/main/java/com/espiritu/tecsupfit/MainActivity.kt
@@ -3,45 +3,16 @@ package com.espiritu.tecsupfit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.espiritu.tecsupfit.ui.theme.TecsupFITTheme
+import androidx.compose.material3.MaterialTheme
+import com.espiritu.tecsupfit.ui.screens.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TecsupFITTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme {
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TecsupFITTheme {
-        Greeting("Android")
     }
 }
